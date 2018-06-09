100c38cc050b0ae17081f19fe1df7ba66a85c7eb
James <jamescraig@finervision.com>
2018-06-09 20:20:39 +0100

    Converted

--- src/assets/js/Containers/AppContainer.js
+++ src/assets/js/Containers/AppContainer.js
@@ -1,23 +1,19 @@
-import React, {Component} from "react";
+import React from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import AboutPage from "../Pages/AboutPage";
 import ContactPage from "../Pages/ContactPage";
 import BlogPage from "../Pages/BlogPage";
 
-export default class AppContainer extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route exact path="/about" component={AboutPage}/>
-                <Route exact path="/contact" component={ContactPage}/>
-                <Route exact path="/blog" component={BlogPage}/>
+export default () => (
+    <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/about" component={AboutPage}/>
+        <Route exact path="/contact" component={ContactPage}/>
+        <Route exact path="/blog" component={BlogPage}/>
 
-                <Route>
-                    <Redirect to="/"/>
-                </Route>
-            </Switch>
-        );
-    }
-}
+        <Route>
+            <Redirect to="/"/>
+        </Route>
+    </Switch>
+);
