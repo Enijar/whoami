10f1c6e1b467437f810afeadcbd131900589ac3d
James <jamescraig@finervision.com>
2018-06-09 19:15:54 +0100

    Added pages to router.

--- src/assets/js/Containers/AppContainer.js
+++ src/assets/js/Containers/AppContainer.js
@@ -1,14 +1,18 @@
 import React, {Component} from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 import HomePage from "../Pages/HomePage";
+import AboutPage from "../Pages/AboutPage";
 import ContactPage from "../Pages/ContactPage";
+import BlogPage from "../Pages/BlogPage";
 
 export default class AppContainer extends Component {
     render() {
         return (
             <Switch>
                 <Route exact path="/" component={HomePage}/>
+                <Route exact path="/about" component={AboutPage}/>
                 <Route exact path="/contact" component={ContactPage}/>
+                <Route exact path="/blog" component={BlogPage}/>
 
                 <Route>
                     <Redirect to="/"/>
