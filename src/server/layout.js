36a150f424c1e6f22d264c6ba3e706815ee05b80
James Craig <jamescraig@finervision.com>
2018-06-11 08:54:15 +0100

    Fixed poor scale meta attribute

--- src/server/layout.js
+++ src/server/layout.js
@@ -6,7 +6,7 @@ module.exports = ({body, title = 'whoami'}) => `
         <meta charset="UTF-8">
         <meta name="author" content="">
         <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>${title}</title>
         
